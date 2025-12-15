1f50173f2771af7fdf73b39087dd939189d7eaae
joeld <dev@joeldermont.fr>
2025-12-15 15:35:18 +0100

    Modifications

--- setup-server.sh
+++ setup-server.sh
@@ -36,7 +36,7 @@ if ! command -v docker >/dev/null; then
   apt install -y ca-certificates curl gnupg lsb-release
   install -m 0755 -d /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  echo "deb [signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian trixie stable" \
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" \
     > /etc/apt/sources.list.d/docker.list
   apt update
   apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
@@ -103,7 +103,6 @@ iptables -F DOCKER-USER
 iptables -A DOCKER-USER -j RETURN
 
 iptables -I FORWARD 1 -j DOCKER-USER
-iptables -I FORWARD 2 -j DOCKER-FORWARD
 
 iptables -A FORWARD -i docker0 -j ACCEPT
 iptables -A FORWARD -o docker0 -j ACCEPT
